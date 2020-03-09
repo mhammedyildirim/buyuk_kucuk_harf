bd4d2847145562bb55b72dfebe72e041d3282023
mhammedyildirim <61433710+mhammedyildirim@users.noreply.github.com>
2020-03-10 00:12:23 +0300

    Initial commit

---
+++ buyuk_kucuk_harf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buyuk_kucuk_harf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir harf giriniz: ");
+            var harf = Convert.ToChar((Console.ReadLine()));
+
+            if (harf >= 65 && harf <= 90)
+            {
+                Console.WriteLine("Girilen harf büyüktür.");
+            }
+            else if (harf >= 97 && harf <= 122) {
+
+                Console.WriteLine("Girilen harf küçüktür.");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
